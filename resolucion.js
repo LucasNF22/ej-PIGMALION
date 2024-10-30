dcfe2078315bf19fa31bee01526f8d73f5d856fa
LucasNF22 <lucasnfiorentino@gmail.com>
2024-10-30 17:28:01 -0300

    Limpieza de codigo

--- resolucion.js
+++ resolucion.js
@@ -1,62 +1,68 @@
-const array = [ 6, 4, 20, 78, 45, 30, 87, 15, 13 ,51, 51, 54, 81, 64, 3, 2  ]
-const requiredNum = 5
+
+let array = [ 6, 4, 20, 78, 45, 30, 87, 15, 13 ,51, 51, 54, 81, 64, 3, 2  ]
+let requiredNum = 5
+let requiredNum2 = 20
 
 
 /*
 El primer ejercicio se resuelve iterando sobre todos los elementos sin tener en cuenta iteraciones 
 repetidas y asumiendo que la suma tiene que ser sobre dos elementos distintos
+ 
+   funcion buscar ( array, buscado )
+
+      para (indice A) desde (0) hasta (largo de arrary)
+
+         para (indice B) desde (0) hasta (largo de array)
+         
+            si ( a distinto de b ) y (num[A] + num[B] === a buscado)
+               retornar true
+
+      sino retornar false
+
+
 */
 
-/*  
 
-funcion buscar ( array, buscado )
-   para (indice A) desde (0) hasta (largo de arrary)
-      para (indice B) desde (0) hasta (largo de array)
-         si ( a distinto de b ) y (num[A] + num[B] === a buscado)
-            retornar true
-   sino retornar false
+let buscar = ( array, buscado ) => {
 
-*/
+   for( let a=0 ; a<array.length ; a++ ){
 
+      for( let b=0 ; b<array.length ; b++ ){
 
-let busca = ( array, buscado ) => {
-
-    for( let a=0 ; a<array.length ; a++ ){
-
-        for( let b=0 ; b<array.length ; b++ ){
-
-            if ( (a != b) && ((array[a] + array[b]) === buscado) ) {
-               
-               return true                      
-            }    
-        }
-    }
-    return false
+         if ( (a != b) && ((array[a] + array[b]) === buscado) ) {      
+            return true                      
+         }    
+      }
+   }   
+   return false
 }
 
+// -------------------------------------------------------------------------------------
 /*
-En el segundo ejercicio suponemos que los numeros son enteros y positivos.
-Se crea un nuevo array filtrando elementos que sea
+En el segundo ejercicio suponemos que los números son enteros y positivos.
+Se crea un nuevo array filtrando elementos que sean mayores al número buscado para descartar cálculos innecesarios.
+Luego se mediante un for of se itera el array, comparando si el valor que resta para llegar al numero buscado está dentro 
+de un array creado para almacenar los elementos que ya se iteraron, evitando así iteraciones duplicadas.
+En el caso que el número esté en el array se retorna true y sino se almacena ese número en el array para compararlo en 
+la próxima iteración del ciclo.
+
+   funcion buscar2 ( array, buscado )     
+      filtrados = array filtrado (nums <= a numero)                
+      iniciar arrayUsados vacio                                   
+                                                                   
+      para num en nuevoArray (for of)
+         resto = buscado - num
+
+         si ( resto esta en arrayUsados )
+            retornar true
+         agregar resto a arrayUsados    
+                  
+      retornar false
 
 */
 
-//  funcion buscar2 ( array, buscado )                                     
-//      filtrados = array filtrado (nums <= a numero)                
-//      iniciar arrayUsados vacio                                   
-//                                                                   
-//      para num en nuevoArray (for of)
-//          resto = buscado - num
-//          si ( resto esta en arrayUsados )
-//              retornar true
-//          agregar resto a arrayUsados    
-//                  
-//      retornar false
-
-let busca4 = ( array, buscado ) => {
-
-   let filtrados = array.filter(nums => nums <= buscado )
-   console.log(filtrados);
-   
+let buscar2 = ( array, buscado ) => {
+   let filtrados = array.filter(nums => nums <= buscado ) 
    let arrayUsados = []
    
    for( num of filtrados ){
@@ -65,42 +71,14 @@ let busca4 = ( array, buscado ) => {
       if( arrayUsados.includes(resto) ){
          return true
       }
-
       arrayUsados.push(num)
       
-   }
-   console.log(arrayUsados);
-   
-   return false
-       
+   }   
+   return false      
 }
-   
+//------------------------------------------------------------  
 
-
-
-
-
-
-let letras = ["a","b","c","d"]
-let busca6 = ( letras, numero ) => {
-
-   for( let a=0 ; a<letras.length ; a++ ){
-
-       for( let b=0 ; b<letras.length   ; b++ ){
-
-           if (a!=b){
-
-              console.log(`${letras[a]} + ${letras[b]}`);
-           } 
-
-           
-           
-       }
-
-   }
-   return false
-}
-
-
-// console.log(busca6(letras, requiredNum))
-console.log(busca(array, requiredNum))
+console.log(buscar(array, requiredNum))
+console.log(buscar2(array, requiredNum))
+console.log(buscar(array, requiredNum2))
+console.log(buscar2(array, requiredNum2))
